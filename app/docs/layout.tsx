678163d781f8de46b1b1e1a1a82f1bfa118c1b62
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-18 10:35:08 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,6 +1,7 @@
 import { DocsLayout } from 'next-docs-ui/layout'
 import type { ReactNode } from 'react'
 import { tree } from '../source'
+import { Nav } from "next-docs-ui/nav";
 import 'next-docs-ui/style.css'
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
