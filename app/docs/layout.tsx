621f471546e1fab1243a73d8325d5aa9d725bd9e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-18 10:31:19 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -5,7 +5,22 @@ import 'next-docs-ui/style.css'
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-          <DocsLayout tree={tree} nav={{ title: '雙龍體育幫助中心' }}  enableSidebar={pathname.startsWith('/docs/') || pathname === '/docs'}>
+      <DocsLayout
+        enableSidebar={pathname.startsWith('/docs/') || pathname === '/docs'}
+            tree={tree}
+            nav={{
+              component: (
+                <Nav
+                  title={<>雙龍體育幫助中心</>}
+                  collapsibleSidebar={false}
+                  items={[{ href: "/support", children: "返回幫助中心" }]}
+                  
+                  ]}
+                />
+              ),
+            }}
+          >
+       
       {children}
     </DocsLayout>
     </div>
