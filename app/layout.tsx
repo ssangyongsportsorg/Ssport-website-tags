78e43831a65fb1c2b29aa84cf08acc8e43c9a603
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-18 10:45:59 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { RootProvider } from 'next-docs-ui/provider'
 import { Inter } from 'next/font/google'
 import type { ReactNode } from 'react'
 import 'next-docs-ui/style.css'
-
+import '@/styles/globals.css'
 import Footer from '@/pages/components/footer'
 const inter = Inter({
   subsets: ['latin']
