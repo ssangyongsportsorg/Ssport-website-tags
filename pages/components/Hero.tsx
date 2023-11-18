5f8eed627ba08997b7999533bf40df56f68709ee
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-18 17:39:40 +0800

    Update Hero.tsx

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -8,7 +8,6 @@ function hero() {
     <Link
       href="/blog/status"
       className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-      role="alert"
     >
       <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
         新
