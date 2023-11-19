fa7faa6edbfcdf3921a56b657e6c331c9f6ee16f
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-19 21:32:15 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,14 +1,15 @@
+'use client'
+
 import { DocsLayout } from 'next-docs-ui/layout'
 import type { ReactNode } from 'react'
 import { tree } from '../source'
 import { Nav } from "next-docs-ui/nav";
 import 'next-docs-ui/style.css'
-import Head from '@/pages/components/head'
-'use client'
+import Helpheader from '@/pages/components/helpheader'
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
  <div>
-         <Head />
+         <Helpheader />
 
   <DocsLayout
     
