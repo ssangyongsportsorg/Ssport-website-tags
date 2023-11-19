232666548924f34976fe603505d27a9eb973a581
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-19 21:49:08 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,14 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="zh-tw" className={inter.className}>
       <body>
-        <RootProvider>{children}</RootProvider>
+        <RootProvider       
+          search={{
+         <DocSearch
+          apiKey="c2e792c2e75fe1dd3e40574f8b4c9a80"
+          appId="70GEOCJCSX"
+          indexName="help"
+        />
+        >{children}</RootProvider>
       </body>
       <Footer />
     </html>
