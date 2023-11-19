f9a35f87c71615a9c39a805257aee54870f9b560
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-19 17:26:03 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -5,8 +5,25 @@ import { Nav } from "next-docs-ui/nav";
 import 'next-docs-ui/style.css'
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
- <div>   
-   {children}
+ <div>
+  <DocsLayout
+    
+    tree={tree}
+sidebar={{
+  collapsible: false
+}}
+    nav={{
+      component: (
+        <Nav
+          title={<>雙龍體育幫助中心</>}
+          url="/support"
+          items={[{ href: "/", children: "返回官網" }]}
+        />
+      ),
+    }}
+  >
+    {children}
+  </DocsLayout>
 </div>
   )
 }
