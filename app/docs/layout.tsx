3aa350786c6b3791afd0047afa3b48912920404f
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-19 21:30:01 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -4,7 +4,7 @@ import { tree } from '../source'
 import { Nav } from "next-docs-ui/nav";
 import 'next-docs-ui/style.css'
 import Head from '@/pages/components/head'
-
+'use client'
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
  <div>
