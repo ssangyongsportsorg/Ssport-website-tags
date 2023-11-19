cf78e962680a15742ec8445622ba20003aa8619c
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-19 21:25:12 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,9 +3,13 @@ import type { ReactNode } from 'react'
 import { tree } from '../source'
 import { Nav } from "next-docs-ui/nav";
 import 'next-docs-ui/style.css'
+import Helpheader from '@/app/components/helpheader'
+
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
  <div>
+         <Helpheader />
+
   <DocsLayout
     
     tree={tree}
