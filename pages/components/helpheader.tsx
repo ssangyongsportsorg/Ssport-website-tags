52218a35812e26899ab390294a6ed6aa1cab38ac
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-19 19:54:10 +0800

    Update helpheader.tsx

--- pages/components/helpheader.tsx
+++ pages/components/helpheader.tsx
@@ -35,14 +35,14 @@ export default function Header() {
     <Navbar.Toggle />
   </div>
   <Navbar.Collapse>
-    <a
-      href="https://slb.ssangyongsports.org"
+    <Link
+      href="/"
        className="block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
     >
-      SLB
+      返回官網
     </a>
-    <a href="https://sba.ssangyongsports.org" className="block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
-      SBA
+    <Link href="/contact" className="block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
+      聯繫
     </a>
   </Navbar.Collapse>
  
