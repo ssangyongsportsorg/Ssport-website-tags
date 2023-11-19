953840714ac0b5f75260e48aa93f2e2fdf8dba42
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-19 19:51:45 +0800

    Update helpheader.tsx

--- pages/components/helpheader.tsx
+++ pages/components/helpheader.tsx
@@ -1,12 +1,9 @@
 import { Navbar } from "flowbite-react";
-import Login from "./login";
 import React from 'react';
 import Link from 'next/link';
-import { signIn, signOut, useSession } from "next-auth/react"
-import { Session } from "next-auth"
 import { DocSearch } from '@docsearch/react'
 import '@docsearch/css';
-export default function helphead() {
+export default function Header() {
   return (
     <>
     <header>
@@ -16,7 +13,7 @@ export default function helphead() {
   rounded={true}
     className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
 >
-    <Link href="/support">
+    <Link href="/">
     <img
       src="/logo.png"
       className="mr-3 h-6 sm:h-9"
@@ -39,15 +36,14 @@ export default function helphead() {
   </div>
   <Navbar.Collapse>
     <a
-      href="/"
+      href="https://slb.ssangyongsports.org"
        className="block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
     >
-      返回官網
+      SLB
     </a>
     <a href="https://sba.ssangyongsports.org" className="block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
-            聯繫我們
+      SBA
     </a>
-   
   </Navbar.Collapse>
  
 </Navbar>
