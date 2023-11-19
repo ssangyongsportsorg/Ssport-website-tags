7ccf070bc5cca5312c52b6c8607be13ab614930e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-19 10:11:20 +0800

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,15 +1,11 @@
-import { languages } from '@/app/i18n'
-import { getPages, getPageUrl } from '@/app/source'
-import { createI18nSearchAPI } from 'next-docs-zeta/server'
- 
-export const { GET } = createI18nSearchAPI('simple', {
-  indexes: languages.map(lang => {
-    const pages = getPages(lang)!.map(page => ({
-      title: page.title,
-      content: page.body.raw,
-      url: getPageUrl(page.slug, lang)
-    }))
- 
-    return [lang, pages]
-  })
+import { getPageUrl } from '@/app/source'
+import { allDocs } from 'contentlayer/generated'
+import { createSearchAPI } from 'next-docs-zeta/search/server'
+
+export const { GET } = createSearchAPI('simple', {
+  indexes: allDocs.map(page => ({
+    title: page.title,
+    content: page.body.raw,
+    url: getPageUrl(page.slug)
+  }))
 })
