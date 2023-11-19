12495c885dd5a5f3f8a8efe5a156f676313eb670
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-19 22:06:07 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import type { ReactNode } from 'react'
 import 'next-docs-ui/style.css'
 import '@/styles/globals.css'
-import '@docsearch/css';
 
 
 import Footer from '@/pages/components/footer'
@@ -11,7 +10,6 @@ import Footer from '@/pages/components/footer'
 const inter = Inter({
   subsets: ['latin']
 })
-const SearchDialog = dynamic(() => import('@/app/components/search'))
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
