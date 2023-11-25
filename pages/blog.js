2141cbc060dacd2246e2f37e3af6b3a0641d7f1d
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-25 22:25:31 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -82,22 +82,10 @@ export default function Blog({posts}){
             </span>
           </a>
           <Link
-            className="inline-flex items-center font-medium text-blue-600 hover:underline dark:text-blue-500"
             href={`/blog/${slug}`}
           >
                     閱讀文章
-            <svg
-              className="w-4 h-4 ml-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
+          
           </Link>
         </div>
       </article>
