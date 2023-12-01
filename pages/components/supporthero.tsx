f8124d9226e3169a31c34f7f2780c6158b7a7655
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-12-01 22:29:03 +0800

    Update supporthero.tsx

--- pages/components/supporthero.tsx
+++ pages/components/supporthero.tsx
@@ -4,14 +4,16 @@ import a from './docsearch.module.css';
 import '@docsearch/css';
 
 
-function supporthero() {
-  return (
+export default function supporthero() {
+const { data: session, status } = useSession();
+if (session) {
+return (
 <section
   className="bg-gradient-to-br from-indigo-50 via-white to-cyan-100"
 >
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-      需要 <span className={styles.abc}>雙龍體育</span>的幫助嗎?
+   {session.user.name}   你好👋， 需要 <span className={styles.abc}>雙龍體育</span>的幫助嗎?
     </h2>
         <div className={`${a.docSearch} ${a.container}`}>
     <DocSearch
@@ -22,8 +24,5 @@ function supporthero() {
           </div>    
   </div>
 </section>
-
-  )
+)
 }
-
-export default supporthero;
