dbf6b85eefde725dc7d9c5b8427202cd504f0143
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-12-02 10:08:45 +0800

    Update supporthero.tsx

--- pages/components/supporthero.tsx
+++ pages/components/supporthero.tsx
@@ -1,31 +1,29 @@
-'use client';
-import React from 'react';
-import { useSession } from "next-auth/react"
 import styles from './Button.module.css';
 import { DocSearch } from '@docsearch/react'
 import a from './docsearch.module.css';
 import '@docsearch/css';
 
-export default function Supporthero() {
-  const { data: session, status } = useSession();
-  if (session) {
-    return (
-      <>
-        <section className="bg-gradient-to-br from-indigo-50 via-white to-cyan-100">
-          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-            <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-              {session.user.name} 你好👋，需要 <span className={styles.abc}>雙龍體育</span> 的幫助嗎?
-            </h2>
-            <div className={`${a.docSearch} ${a.container}`}>
-              <DocSearch
-                apiKey="c2e792c2e75fe1dd3e40574f8b4c9a80"
-                appId="70GEOCJCSX"
-                indexName="help"
-              />
-            </div>
-          </div>
-        </section>
-      </>
-    );
-  }
+
+function supporthero() {
+  return (
+<section
+  className="bg-gradient-to-br from-indigo-50 via-white to-cyan-100"
+>
+  <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+    <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      需要 <span className={styles.abc}>雙龍體育</span>的幫助嗎?
+    </h2>
+        <div className={`${a.docSearch} ${a.container}`}>
+    <DocSearch
+              apiKey="c2e792c2e75fe1dd3e40574f8b4c9a80"
+              appId="70GEOCJCSX"
+              indexName="help"
+            />
+          </div>    
+  </div>
+</section>
+
+  )
 }
+
+export default supporthero;
