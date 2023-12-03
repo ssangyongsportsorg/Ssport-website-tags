26b7ec96a2be181f75a172e9f2670ef80b70582c
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-12-03 10:33:42 +0800

    Update layout.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,11 +1,16 @@
+'use client'
+
 import { DocsLayout } from 'next-docs-ui/layout'
 import type { ReactNode } from 'react'
 import { tree } from '../source'
 import { Nav } from "next-docs-ui/nav";
 import 'next-docs-ui/style.css'
+import Helpheader from '@/app/components/helpheader'
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return (
-<div>
+ <div>
+         <Helpheader />
+
   <DocsLayout
     
     tree={tree}
@@ -13,13 +18,8 @@ sidebar={{
   collapsible: false
 }}
     nav={{
-      component: (
-        <Nav
-          title={<>雙龍體育幫助中心</>}
-          url="/support"
-          items={[{ href: "/", children: "返回官網" }]}
-        />
-      ),
+        enabled: false
+      
     }}
   >
     {children}
