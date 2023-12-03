04a654479a2bc34697a3dde101d1675111bd921c
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-03 21:23:53 +0800

    Update support.tsx

--- pages/support.tsx
+++ pages/support.tsx
@@ -1,5 +1,6 @@
 import Supportchoice from './components/supportchoice';
 import Supporthero from './components/supporthero';
+import Supportend from './components/supportend';
 import styles from './components/docsearch.module.css';
 import Ticket from './components/ticket';
 import Head from 'next/head'
@@ -12,15 +13,11 @@ const support = () => {
 <Head>
         <title>雙龍體育幫助</title>
         <meta name="description" content="歡迎來到雙龍體育幫助中心,任何問題立刻聯繫" />
-      </Head>
-      
+      </Head>      
       <div>
-
-                        <Supporthero />
-
-
+        <Supporthero />
         <Supportchoice />
-             
+       <Supportend />             
       </div>
     </>
       );
