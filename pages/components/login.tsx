0b14ea07ea90e8dc362ef2faa0bd7775c4fdebe7
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-12-03 10:50:11 +0800

    Update login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -9,7 +9,7 @@ const { data: session, status } = useSession();
 if (session) {
 return (
 <>
-<Dropdown inline label={<Avatar alt="User settings" img={`${session?.user.image}`} rounded/>} > <Dropdown.Header> <span className="block text-sm"> {session.user.name}  </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <div> 尚未開放 </div> <div> 尚未開放 </div> <div> 尚未開放 </div> <Dropdown.Divider /> <div>
+<Dropdown inline label={<Avatar alt="User settings" img={`${session?.user.image}`} rounded/>} > <Dropdown.Header> <span className="block text-sm"> {session.user.name}  </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <div> <Link href="/watch-tv">觀看TV</Link></div> <div> <Link href="#">購物</Link> </div> <div> <Link href="/support">幫助</Link> </div> <Dropdown.Divider /> <div>
   <Link href={`/api/auth/signout`} 
                  onClick={(e) => { 
                    e.preventDefault() 
