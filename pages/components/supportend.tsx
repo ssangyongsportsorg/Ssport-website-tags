b523bab0b66d2d92722864cf7f22e5711da08c77
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-03 21:26:36 +0800

    Update supportend.tsx

--- pages/components/supportend.tsx
+++ pages/components/supportend.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import styles from './Button.module.css';
 function supportend() {
   return (
-<section className="bg-white dark:bg-gray-900">
+<section>
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-md sm:text-center">
       <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">
