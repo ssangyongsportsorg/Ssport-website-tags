b779115b6f9ad306fe779f561b5a8066b8063adb
BRIAN <107471917+sxadxsx@users.noreply.github.com>
2023-12-09 22:07:17 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-import createNextDocsMDX from 'next-docs-mdx/config'
- 
-/** @type {import('next').NextConfig} */
-const config = {
-  reactStrictMode: true
-}
-export default withFumaMDX(config)
-
 const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
